2b809c80a4c7f3ccf57cfe4902e87b66f9c3e0e7
Maximo Zoppini <maximozoppini@gmail.com>
2022-09-08 09:47:33 -0300

    agrego llamada a airtable

--- server/index.js
+++ server/index.js
@@ -1,5 +1,8 @@
 const express = require("express");
 require("dotenv").config();
+const airTable = require("airtable");
+airTable.configure({ apiKey: "key4NlM7Rxi1Tc43V" });
+const base = require("airtable").base("appAPeVcmiZuk0Kl2");
 
 const app = express();
 const server = app.listen(process.env.PORT || 8082, () => {
@@ -8,4 +11,58 @@ const server = app.listen(process.env.PORT || 8082, () => {
 
 app.get("/", (req, res) => {
     res.send("hola mundo");
+});
+
+app.get("/barrios", (req, res) => {
+    base("Barrios")
+        .select({
+            // Selecting the first 3 records in Grid view:
+            maxRecords: 3,
+            pageSize: 5,
+            view: "Grid view",
+        })
+        .eachPage(
+            function page(records, fetchNextPage) {
+                // This function (`page`) will get called for each page of records.
+
+                // records.forEach(function (record) {
+                //     console.log("Retrieved", record.id);
+                // });
+                res.json(records);
+
+                // To fetch the next page of records, call `fetchNextPage`.
+                // If there are more records, `page` will get called again.
+                // If there are no more records, `done` will get called.
+                fetchNextPage();
+            },
+            function done(err) {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
+            }
+        );
+});
+
+app.get("/crearDepto", (req, res) => {
+    base("Departamentos").create(
+        [
+            {
+                fields: {
+                    Barrio: "recmf9LyO597z1ddo",
+                },
+            },
+        ],
+        { typecast: true },
+        function (err, records) {
+            if (err) {
+                console.error(err);
+                return;
+            }
+            records.forEach(function (record) {
+                // console.log(record.getId());
+                res.json(record.getId());
+            });
+        }
+    );
 });
